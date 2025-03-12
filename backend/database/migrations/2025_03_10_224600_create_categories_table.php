e22aa8f1a7fa11f68ec771fa1ccbf946252089d9
Ali Yara <ali.yara.fr@gmail.com>
2025-03-12 12:44:57 +0000

    update categories table

--- backend/database/migrations/2025_03_10_224600_create_categories_table.php
+++ backend/database/migrations/2025_03_10_224600_create_categories_table.php
@@ -5,15 +5,14 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    /**
-     * Run the migrations.
-     */
+    
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('name');
             $table->string('icon')->nullable();
+            $table->uuid("parent_id")->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
