af45a3175c84db2339139c04092e34affef7c2de
Ali Yara <ali.yara.fr@gmail.com>
2025-03-12 12:39:48 +0000

    add Swagger annotations

--- backend/app/Http/Controllers/V1/CourseController.php
+++ backend/app/Http/Controllers/V1/CourseController.php
@@ -7,38 +7,149 @@ use App\Http\Requests\V1\StoreCourseRequest;
 use App\Http\Requests\V1\UpdateCourseRequest;
 use App\Interfaces\CourseInterface;
 use App\Models\Course;
+use OpenApi\Annotations as OA;
 
 class CourseController extends Controller
 {
     private $courseInterface;
 
-    public function __construct(CourseInterface $course) {
+    public function __construct(CourseInterface $course)
+    {
         $this->courseInterface = $course;
     }
 
+
+    /**
+     * @OA\Get(
+     * path="/api/courses",
+     * summary="Get a list of courses",
+     * tags={"Course"},
+     * @OA\Response(response=200, description="Successful operation"),
+     * @OA\Response(response=400, description="Invalid request")
+     * )
+     */
     public function index()
     {
         return $this->courseInterface->getCourses();
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/courses",
+     *     summary="Store a new course",
+     *     tags={"Course"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"title"},
+     *             required={"description"},
+     *             required={"content"},
+     *             required={"tags"},
+     *             required={"duration"},
+     *             required={"level"},
+     *             required={"categoryId"},
+     *             @OA\Property(property="title", type="string", example=""),
+     *             @OA\Property(property="description", type="string", example=""),
+     *             @OA\Property(property="content", type="string", example=""),
+     *             @OA\Property(property="tags", type="array",  @OA\Items(type="string"), example=""),
+     *             @OA\Property(property="duration", type="decimal", example=""),
+     *             @OA\Property(property="video", type="string", example=""),
+     *             @OA\Property(property="cover", type="string", example=""),
+     *             @OA\Property(property="level", type="string", example="beginner, intermediate, advanced"),
+     *             @OA\Property(property="categoryId", type="string", example=""),
+     *             @OA\Property(property="teacherId", type="string", example="")
+     *         )
+     *     ),
+     *     @OA\Response(response=201, description="Course created"),
+     *     @OA\Response(response=400, description="Invalid request")
+     * )
+     */
+    
     public function store(StoreCourseRequest $request)
     {
         return $this->courseInterface->storeCourse($request);
     }
-
+    /**
+     * @OA\Get(
+     *     path="/api/courses/{id}",
+     *     summary="Get course details",
+     *     tags={"Course"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="Course ID",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(response=200, description="Successful operation"),
+     *     @OA\Response(response=404, description="Course not found")
+     * )
+     */
 
     public function show(Course $course)
     {
         return $this->courseInterface->getCourse($course);
     }
 
-
+    /**
+     * @OA\Put(
+     *     path="/api/courses/{id}",
+     *     summary="Ipdate a new course",
+     *     tags={"Course"},
+     * @OA\Parameter(
+     * name="Course ID",
+     * in="path",
+     * required=true,
+     * description="Course ID",
+     * @OA\Schema(type="integer"),
+     * ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"title"},
+     *             required={"description"},
+     *             required={"content"},
+     *             required={"tags"},
+     *             required={"duration"},
+     *             required={"level"},
+     *             required={"categoryId"},
+     *             @OA\Property(property="title", type="string", example=""),
+     *             @OA\Property(property="description", type="string", example=""),
+     *             @OA\Property(property="content", type="string", example=""),
+     *             @OA\Property(property="tags", type="array",  @OA\Items(type="string"), example=""),
+     *             @OA\Property(property="duration", type="decimal", example=""),
+     *             @OA\Property(property="video", type="string", example=""),
+     *             @OA\Property(property="cover", type="string", example=""),
+     *             @OA\Property(property="level", type="string", example="beginner, intermediate, advanced"),
+     *             @OA\Property(property="categoryId", type="string", example=""),
+     *             @OA\Property(property="teacherId", type="string", example="")
+     *         )
+     *     ),
+     *     @OA\Response(response=201, description="Course updated"),
+     *     @OA\Response(response=400, description="Invalid request")
+     * )
+     */
     public function update(UpdateCourseRequest $request, Course $course)
     {
         return $this->courseInterface->updateCourse($request, $course);
     }
 
-
+    /**
+     * @OA\Delete(
+     *     path="/api/courses/{id}",
+     *     summary="Delete a course",
+     *     tags={"Course"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="Course ID",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(response=200, description="Course deleted"),
+     *     @OA\Response(response=404, description="Course not found")
+     * )
+     */
     public function destroy(Course $course)
     {
         return $this->courseInterface->deleteCourse($course);
--- backend/app/Http/Controllers/V1/TagController.php
+++ backend/app/Http/Controllers/V1/TagController.php
@@ -16,12 +16,37 @@ class TagController extends Controller
     {
         $this->tagInterface = $tagInterface;
     }
+
+     /**
+     * @OA\Get(
+     *     path="/api/tags",
+     *     summary="Get a list of tags",
+     *     tags={"Tags"},
+     *     @OA\Response(response=200, description="Successful operation"),
+     *     @OA\Response(response=400, description="Invalid request")
+     * )
+     */
     public function index()
     {
         return $this->tagInterface->getTags();
     }
 
-   
+   /**
+     * @OA\Post(
+     *     path="/api/tags",
+     *     summary="Store a new Tag",
+     *     tags={"Tags"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"name"},
+     *             @OA\Property(property="name", type="string", example="")
+     *         )
+     *     ),
+     *     @OA\Response(response=201, description="Tag created"),
+     *     @OA\Response(response=400, description="Invalid request")
+     * )
+     */
     public function store(StoreTagRequest $request)
     {
         return $this->tagInterface->storeTag($request);
@@ -33,13 +58,50 @@ class TagController extends Controller
     //     return $this->tagInterface->getTags();
     // }
 
-  
+  /**
+     * @OA\Put(
+     *     path="/api/tags/{id}",
+     *     summary="Update a Tag",
+     *     tags={"Tags"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="Tag ID",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"name"},
+     *             @OA\Property(property="name", type="string", example="")
+     *         )
+     *     ),
+     *     @OA\Response(response=200, description="Tag updated"),
+     *     @OA\Response(response=400, description="Invalid request")
+     * )
+     */
     public function update(UpdateTagRequest $request, Tag $tag)
     {
         return $this->tagInterface->updateTag($request, $tag);
     }
 
-   
+    /**
+     * @OA\Delete(
+     *     path="/api/tags/{id}",
+     *     summary="Delete a Tag",
+     *     tags={"Tags"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="Tag ID",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(response=200, description="Tag deleted"),
+     *     @OA\Response(response=404, description="Tag not found")
+     * )
+     */
     public function destroy(Tag $tag)
     {
         return $this->tagInterface->deleteTag($tag);
