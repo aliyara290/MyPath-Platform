6329a8505cd3bbc3ad50d350dcc76290e601d737
Ali Yara <ali.yara.fr@gmail.com>
2025-04-10 23:48:44 +0100

    update admin pages

--- frontend/src/pages/admin/CoursesManagement.jsx
+++ frontend/src/pages/admin/CoursesManagement.jsx
@@ -260,16 +260,16 @@ const SimpleCourseManagement = () => {
             </div>
 
             <div className="mt-5 flex justify-end space-x-3">
-              <button
+            <button
                 type="button"
                 onClick={handleCancel}
-                className="px-4 py-2 border border-gray-300 rounded-md text-sm text-gray-700 bg-white hover:bg-gray-50"
+                className="px-4 py-2 border cursor-pointer border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
               >
                 Cancel
               </button>
               <button
                 type="submit"
-                className="px-4 py-2 border border-transparent rounded-md text-sm text-white bg-primary-600 hover:bg-primary-700"
+                className="px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 cursor-pointer hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
               >
                 {editingCourse ? 'Update' : 'Create'} Course
               </button>
--- frontend/src/pages/admin/TagsManagement.jsx
+++ frontend/src/pages/admin/TagsManagement.jsx
@@ -215,16 +215,16 @@ const TagsManagement = () => {
             </div>
 
             <div className="mt-6 flex items-center justify-end space-x-3">
-              <button
+            <button
                 type="button"
                 onClick={handleCancel}
-                className="px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
+                className="px-4 py-2 border cursor-pointer border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
               >
                 Cancel
               </button>
               <button
                 type="submit"
-                className="px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
+                className="px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 cursor-pointer hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
               >
                 {editingTag ? 'Update' : 'Create'} Tag
               </button>
